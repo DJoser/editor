b9dd9ff55fa4a97520810b921f0a9acb6260bb8a
Jose Ramon Navarro Marquez <ramonnavarropadilla@hotmail.com>
2016-12-28 14:22:32 -0600

    Se conecto con angular para mostrar notas por defecto

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,3 +66,35 @@ $(function () {
         });
     }
 });
+
+
+
+angular.module('editorApp', [])
+    .controller('notasController', function($scope) {
+        $scope.notas = [
+            {titulo:'learn angular',contenido:"Contenido de la nota 1"},
+            {titulo:'build an angular app', contenido:"Contenido de la nota 2"}
+        ];
+
+        $scope.addNote = function() {
+            $scope.notas.push({text:$scope.todoText, done:false});
+            $scope.todoText = '';
+        };
+
+        $scope.remaining = function() {
+            var count = 0;
+            angular.forEach($scope.notas, function(todo) {
+                count++;
+            });
+            return count;
+        };
+
+        $scope.archive = function() {
+            var oldTodos = $scope.notas;
+            $scope.notas = [];
+            angular.forEach(oldTodos, function(todo) {
+                //if (!todo.done) notasList.notas.push(todo);
+            });
+        };
+    });
+
